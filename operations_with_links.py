c8a7dc4d5b6cb41c3bb7c908a64cd8e1ef5d2057
mikrukovd <z123456689z@gmail.com>
2025-02-09 18:23:05 +0300

    Create operations_with_links.py

---
+++ operations_with_links.py
@@ -0,0 +1,51 @@
+import requests
+from urllib.parse import urlparse
+
+
+def shorten_link(url, token):
+    short = 'https://api.vk.ru/method/utils.getShortLink'
+    params = {
+        'access_token': token,
+        'v': 5.199,
+        'url': url
+    }
+    response = requests.get(short, params=params)
+    response.raise_for_status()
+    short_link = response.json()
+
+    if 'error' in short_link:
+        error_code = short_link['error']['error_code']
+        error_msg = short_link['error']['error_msg']
+        raise requests.exceptions.HTTPError(f'Error {error_code}: {error_msg}')
+
+    return short_link['response']['short_url']
+
+
+def count_clicks(token, link):
+    url = 'https://api.vk.ru/method/utils.getLinkStats'
+    parsed = urlparse(link)
+    key = parsed.path
+    if key[0] == '/':
+        key = key.replace('/', '', 1)
+    params = {
+        'access_token': token,
+        'v': 5.199,
+        'key': key,
+        'extended': 0,
+        'interval': 'forever'
+
+    }
+    response = requests.get(url=url, params=params)
+    response.raise_for_status()
+    clicks_count = response.json()
+
+    if 'error' in clicks_count:
+        error_code = clicks_count['error']['error_code']
+        error_msg = clicks_count['error']['error_msg']
+        raise requests.exceptions.HTTPError(f'Error {error_code}: {error_msg}')
+
+    return clicks_count['response']['stats'][0]['views']
+
+
+def is_shorten_link(url):
+    return 'vk.cc' in url
