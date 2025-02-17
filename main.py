70703d3cefc3f1ab755989c66d04197aabadbae0
mikrukovd <z123456689z@gmail.com>
2025-02-17 10:34:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ from operations_with_links import shorten_link, is_shorten_link, count_clicks
 def main():
     load_dotenv()
     token = os.environ['VK_TOKEN']
-    user_input_url = input('Введите ссылку: ')
+    inputed_url = input('Введите ссылку: ')
 
-    if is_shorten_link(user_input_url, token):
+    if is_shorten_link(inputed_url, token):
         try:
-            print(f'Количество переходов по ссылке: {count_clicks(user_input_url, token)}')
+            print(f'Количество переходов по ссылке: {count_clicks(inputed_url, token)}')
         except requests.exceptions.HTTPError as http_error:
             print(http_error)
     else:
         try:
-            print(f'Короткая ссылка: {shorten_link(user_input_url, token)}')
+            print(f'Короткая ссылка: {shorten_link(inputed_url, token)}')
         except requests.exceptions.HTTPError as http_error:
             print(http_error)
 
